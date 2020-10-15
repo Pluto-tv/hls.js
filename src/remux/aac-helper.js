f2f16165a5359e2d2383570273ece83191f48b2e
Nayan Savla <nayansavla@Gmail.com>
2020-10-15 08:10:27 -0700

    Revert
    reverting the return of null frame since this is related to audio shift and we can use that setting.

--- src/remux/aac-helper.js
+++ src/remux/aac-helper.js
@@ -4,7 +4,6 @@
 
 class AAC {
   static getSilentFrame (codec, channelCount) {
-    return null; //Hisense has issue with silent frames so commenting it out for all platforms as test
     switch (codec) {
     case 'mp4a.40.2':
       if (channelCount === 1) {
